24e34bc4415be7a7acc1caaa651ece0122dcf9ea
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-08-15 17:11:39 +0530

    Add js-mocking-fundamentals from Kent C. Dodds

---
+++ js-mocking-fundamentals/other/jest.no-framework.config.js
@@ -0,0 +1,7 @@
+// module.exports = {
+//   rootDir: path.join(__dirname, '../'),
+//   roots: [path.join(__dirname, '../src')],
+//   displayName: 'no-framework',
+//   testMatch: ['**/no-framework/**/*.js'],
+//   runner: require.resolve('./create-node-runner')
+// }
