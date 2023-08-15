331b997f5740bc90770f6083839a884d8128e69d
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-08-15 17:12:06 +0530

    Update config to run only my learning folder

--- js-mocking-fundamentals/other/jest.no-framework.config.js
+++ js-mocking-fundamentals/other/jest.no-framework.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 // module.exports = {
 //   rootDir: path.join(__dirname, '../'),
 //   roots: [path.join(__dirname, '../src')],
@@ -5,3 +7,11 @@
 //   testMatch: ['**/no-framework/**/*.js'],
 //   runner: require.resolve('./create-node-runner')
 // }
+
+module.exports = {
+  rootDir: path.join(__dirname, '../'),
+  roots: [path.join(__dirname, '../learning')],
+  displayName: 'no-framework',
+  testMatch: ['**/no-framework/**/*.js'],
+  runner: require.resolve('./create-node-runner')
+}
